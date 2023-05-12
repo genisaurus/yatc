a1b4a663f3d528cfae62350bf467ee5ceeec8479
genisaurus <rusty.barron@gmail.com>
2023-05-11 22:35:36 -0400

    posts router initialized and connection to DB validated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import { SignIn, SignUpButton, useUser } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/clerk-react"
 import { type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
-import SignInPage from "./sign-in";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
 
-const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,10 +20,14 @@ const user = useUser();
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
           {!user.isSignedIn && <SignIn />}
+          {!!user.isSignedIn && <SignOutButton />}
         </div>
         <br />
         <div>
           {!user.isSignedIn && <SignUpButton />}
+        </div>
+        <div>
+          {data?.map((post) => ( <div key={post.id}>{post.content}</div> ))}
         </div>
       </main>
     </>
